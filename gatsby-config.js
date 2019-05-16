c8584a4d054401bb0921a24bba17b9c1b29768c4
Nikolay Govorov <nikolay_govorov@bk.ru>
2019-05-16 19:19:19 +0300

    Install Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,5 +128,12 @@ module.exports = {
         production: analyze,
       },
     },
+
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/index.js`,
+      },
+    },
   ],
 };
