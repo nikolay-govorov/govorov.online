ce291ca6a4152129acb89593fb2b0e5e5845ccdb
Nikolay Govorov <nikolay_govorov@bk.ru>
2019-07-26 12:26:33 +0300

    Add the CV to header

--- src/block/navigation/navigation.js
+++ src/block/navigation/navigation.js
@@ -9,6 +9,7 @@ const pages = [
   { title: 'Заметки', url: '/notes/' },
   { title: 'Доклады', url: '/presentations/' },
   { title: 'Желания', url: 'https://notion.so/96248504c9234191ad037b66f02fafbd', external: true },
+  { title: 'Резюме', url: 'https://www.notion.so/Frontend-engineer-0d0ebccf2eb040d8b8360ddde1796983', external: true },
 ];
 
 export default function Navigation() {
