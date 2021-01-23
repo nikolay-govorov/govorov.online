d9c80d40e47158f16f73621471f4f59dda49f5b4
Nikolay Govorov <nikolay_govorov@bk.ru>
2021-01-24 00:15:23 +0300

    Delete old and unused pages, add current job

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,23 +29,7 @@ module.exports = {
         path: path.join(__dirname, 'src', 'pages'),
       },
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              classPrefix: 'language-',
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-        ],
-      },
-    },
 
-    'gatsby-plugin-feed',
-    'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
   ],
 };
--- src/blocks/layout/layout.js
+++ src/blocks/layout/layout.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import cx from 'classnames';
 import Helmet from 'react-helmet';
-import PropTypes from 'prop-types';
+import { withPrefix, Link } from 'gatsby';
 
-import Header from '../header/header';
-import Footer from '../footer/footer';
-import logo from '../../assets/images/logo.png';
-import avatar from '../../assets/images/nikolay_govorov.jpg';
+import logo from '../../images/logo.png';
+import avatarJPG from '../../images/avatar.jpg';
+import avatarWEBP from '../../images/avatar.webp';
+import avatarFull from '../../images/nikolay_govorov.jpg';
+import linkExternal from '../../images/linkExternal.svg';
 
 import meta from '../../../metadata.json';
 
 import '../../styles/main.css';
 import styles from './layout.module.css';
+
+const SIZE = 80;
+const pages = [
+  { title: 'Обо мне', url: '/' },
+  { title: 'Желания', url: 'https://notion.so/96248504c9234191ad037b66f02fafbd', external: true },
+  { title: 'Резюме', url: 'https://www.notion.so/Frontend-engineer-0d0ebccf2eb040d8b8360ddde1796983', external: true },
+];
 
 const AVATAR_SIZE = 1416;
 
@@ -23,8 +32,13 @@ function getCanonical(domain, path) {
 export default function BaseLayout({
   children, location, title,
 }) {
+  const { author: { contacts, name } } = meta;
+
   const pageTitle = title || meta.site.name;
   const canonical = getCanonical(meta.site.url, location.pathname);
+  const year = useMemo(() => new Date().getFullYear(), []);
+
+  const localUrl = withPrefix(location.pathname);
 
   return (
     <>
@@ -45,7 +59,7 @@ export default function BaseLayout({
 
         <meta property="og:url" content={meta.site.url} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content={avatar} />
+        <meta property="og:image" content={avatarFull} />
         <meta property="og:image:width" content={AVATAR_SIZE} />
         <meta property="og:image:height" content={AVATAR_SIZE} />
         <meta property="og:image:type" content="image/jpeg" />
@@ -61,7 +75,70 @@ export default function BaseLayout({
 
       <div className={styles.container}>
         <div className={styles.header}>
-          <Header url={location.pathname} />
+          <header className={styles.header_container}>
+            <div className={styles.header_logo}>
+              <a
+                className={cx('link--clean', styles.logo_link)}
+                href={avatarFull}
+                rel="nofollow noopener noreferrer"
+                target="_blank"
+              >
+                <picture className={styles.logo_avatar}>
+                  <source srcSet={avatarWEBP} type="image/webp" />
+
+                  <img
+                    className={styles.logo_avatar}
+                    src={avatarJPG}
+                    alt="Фото Николая Говорова"
+                    width={SIZE}
+                    height={SIZE}
+                  />
+                </picture>
+              </a>
+            </div>
+
+            <div className={styles.header_navigation}>
+              {localUrl === '/' ? (
+                <h1 className={styles.header_title}>
+                  {meta.site.name}
+                </h1>
+              ) : (
+                  <p className={styles.header_title}>
+                    {meta.site.name}
+                  </p>
+                )}
+
+              <div className={styles.nav_container} aria-label="Страницы сайта">
+                <ul className={styles.nav_list}>
+                  {pages.map(({ title, url, external }) => (
+                    <li className={styles.nav_list__item} key={url}>
+                      {external ? (
+                        <a
+                          href={url}
+                          title={url}
+                          target="_blank"
+                          rel="noopener noreferrer nofollow"
+                          className="link link--not-visited link--external"
+                        >
+                          {` ${title} `}
+
+                          <img src={linkExternal} alt="" aria-label="Ссылка будет открыта в отдельной вкладке" />
+                        </a>
+                      ) : (
+                          <Link
+                            to={url}
+                            className="link link--not-visited"
+                            activeClassName="link--active"
+                          >
+                            {` ${title} `}
+                          </Link>
+                        )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </header>
         </div>
 
         <div className={styles.content}>
@@ -69,15 +146,23 @@ export default function BaseLayout({
         </div>
 
         <div className={styles.footer}>
-          <Footer />
+          <footer className={styles.footer_container}>
+            <div className={styles.contacts_container}>
+              <ul className={styles.contacts_list}>
+                {contacts.map(contact => (
+                  <li className={styles.contacts_list__item} key={contact.url}>
+                    <a rel="me" href={contact.url}>{contact.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <span className="small">
+              &copy; {`2015-${year}, ${name}`}
+            </span>
+          </footer>
         </div>
       </div>
     </>
   );
 }
-
-BaseLayout.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.element.isRequired,
-  location: PropTypes.string.isRequired,
-};
